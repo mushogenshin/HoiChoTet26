23a207518f7230fd0bdec561ae8a3a7f80df4247
Hoan Nguyen <33324682+mushogenshin@users.noreply.github.com>
2026-01-17 20:01:24 +0700

    initialize UE 5.7 C++ project

---
+++ HoiChoTet26/Source/HoiChoTet26/HoiChoTet26.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "HoiChoTet26.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HoiChoTet26, "HoiChoTet26" );
