22a4028daa424a14643cb1ab688218989838aa82
Hoan Nguyen <33324682+mushogenshin@users.noreply.github.com>
2026-01-17 20:22:05 +0700

    thêm các base class cho GameMode, PlayerController

---
+++ HoiChoTet26/Source/HoiChoTet26/Public/HCTPlayerControllerBase.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "HCTPlayerControllerBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HOICHOTET26_API AHCTPlayerControllerBase : public APlayerController
+{
+	GENERATED_BODY()
+};
