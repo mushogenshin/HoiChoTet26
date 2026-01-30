1c556df69f07496c86f5703c24388fb5feb2cae5
Hoan Nguyen <33324682+mushogenshin@users.noreply.github.com>
2026-01-30 15:20:37 +0700

    EjectGame logic: unpossess current Pawn if it's not the original HCTPawn, then repossess the original HCTPawm

--- HoiChoTet26/Source/HoiChoTet26/Private/HCTPlayerControllerBase.cpp
+++ HoiChoTet26/Source/HoiChoTet26/Private/HCTPlayerControllerBase.cpp
@@ -1,6 +1,5 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #include "HoiChoTet26/Public/HCTPlayerControllerBase.h"
+#include "HoiChoTet26/Public/HCTPawnBase.h"
 #include "EnhancedInputComponent.h"
 #include "EnhancedInputSubsystems.h"
 
@@ -33,12 +32,48 @@ void AHCTPlayerControllerBase::SetupInputComponent()
 		if (LookInputAction){
 			EnhancedInputComponent->BindAction(LookInputAction, ETriggerEvent::Triggered, this, &AHCTPlayerControllerBase::Look);
 		}	
+		
+		// Bind the eject game action
+		if (EjectGameInputAction){
+			EnhancedInputComponent->BindAction(EjectGameInputAction, ETriggerEvent::Triggered, this, &AHCTPlayerControllerBase::EjectGame);
+		}
+	}
+}
+
+void AHCTPlayerControllerBase::AcknowledgePossession(APawn* P)
+{
+	Super::AcknowledgePossession(P);
+	
+	// Store reference to the first HCTPawnBase we possess
+	if (!OriginalPawn)
+	{
+		OriginalPawn = Cast<AHCTPawnBase>(P);
 	}
 }
 
 void AHCTPlayerControllerBase::Look(const FInputActionValue& Value)
 {
 	const FVector2D LookAxis = Value.Get<FVector2D>();
+	// NOTE: on the Pawn, bUseControllerRotationYaw must be true to 
+	// allow controller rotation to affect the pawn's camera
 	AddYawInput(LookAxis.X);
 	AddPitchInput(LookAxis.Y);
+}
+
+void AHCTPlayerControllerBase::EjectGame()
+{
+	if (OriginalPawn && GetPawn() == OriginalPawn)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("EjectGame: Already possessing the original pawn."));
+		return;
+	}
+	
+	// Unpossess current pawn from the mini-game
+	UnPossess();
+	
+	// Try repossessing the original pawn 
+	if (OriginalPawn)
+	{
+		Possess(OriginalPawn);
+	}
 }
