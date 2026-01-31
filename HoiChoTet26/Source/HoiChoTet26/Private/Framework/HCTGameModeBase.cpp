c8416c842371b0ef8c05d649f73adda36e229f3d
Hoan Nguyen <33324682+mushogenshin@users.noreply.github.com>
2026-01-31 07:39:22 +0700

    set default classes

--- HoiChoTet26/Source/HoiChoTet26/Private/Framework/HCTGameModeBase.cpp
+++ HoiChoTet26/Source/HoiChoTet26/Private/Framework/HCTGameModeBase.cpp
@@ -1,15 +1,18 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Framework/HCTGameModeBase.h"
+#include "Framework/HCTHUD_Base.h"
+#include "Framework/HCTPawnBase.h"
+#include "Framework/HCTPlayerControllerBase.h"
 
 AHCTGameModeBase::AHCTGameModeBase()
 {
+	// Set default classes
+	PlayerControllerClass = AHCTPlayerControllerBase::StaticClass();
+	DefaultPawnClass = AHCTPawnBase::StaticClass();
+	HUDClass = AHCTHUD_Base::StaticClass();
 }
 
 void AHCTGameModeBase::BeginPlay()
 {
 	Super::BeginPlay();
-	
 	UE_LOG(LogTemp, Warning, TEXT("Bat dau xai Game Mode HoiChoTet"));
 }
